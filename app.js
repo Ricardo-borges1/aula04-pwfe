d30a8a73afa82217e857586f4f9e5ba6b1cb5247
RICARDO BORGES DO AMARAL <23111826@CFP127ALUNO.LOCAL>
2023-08-25 11:30:57 -0300

    quase finalizado

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const botaoMostrarNumeros = document.getElementById('botao-mostrar-numeros')
 const botaoMostrarNumerosPares = document.getElementById('botao-mostrar-numeros-pares')
 const botaoMostrarDobro = document.getElementById('botao-mostrar-dobro')
 const botaoMostrarPar = document.getElementById('botao-mostrar-proximo-par')
+const botaoMostrarNumerosMultiplos3Ou5 = document.getElementById('botao-mostrar-numeros-multiplos-3-ou-5')
+const botaoMostrarNumerosMultiplos3E5 = document.getElementById('botao-mostrar-numeros-multiplos-3-e-5')
+const botaoPrimo = document.getElementById('botao-primo')
 
 function mostrarNumeros() {
     const numeros = document.getElementById('numeros').value.split(',')
@@ -69,9 +72,62 @@ function mostrarProximoPar () {
     }
 }
 
+function mostrarMultiplos3Ou5 () {
+    const numeros = document.getElementById('numeros').value.split(',')
+    const container = document.getElementById('container-numeros-multiplos-3-ou-5')
+
+    container.replaceChildren('')
+    const ultimoIndice = numeros.length
+
+    for (let indice = 0; indice < ultimoIndice; indice++ ){
+
+        if(numeros[indice] % 3 == 0 || numeros[indice] % 5 == 0){
+            const novoSpan = document.createElement('span')
+            novoSpan.textContent = numeros[indice]
+            container.appendChild(novoSpan)
+        }
+    }
+}
+
+function mostrarMultiplos3E5 () {
+    const numeros = document.getElementById('numeros').value.split(',')
+    const container = document.getElementById('container-numeros-multiplos-3-e-5')
+
+    container.replaceChildren('')
+    const ultimoIndice = numeros.length
+
+    for (let indice = 0; indice < ultimoIndice; indice++ ){
+
+        if(numeros[indice] % 3 == 0 && numeros[indice] % 5 == 0){
+            const novoSpan = document.createElement('span')
+            novoSpan.textContent = numeros[indice]
+            container.appendChild(novoSpan)
+        }
+    }
+}
+
+function mostrarPrimo() {
+    const numeros = document.getElementById('numeros').value.split(',')
+    const container = document.getElementById('container-primo')
+
+    container.replaceChildren('')
+    const ultimoIndice = numeros.length
+
+    for (let contador = 0; contador < ultimoIndice; contador++) {
+        if (numeros[contador] % 1 == 0 && numeros[contador] % valor == 0) {
+            const novoSpan = document.createElement('span')
+            novoSpan.textContent = numeros[contador] + 1
+            container.appendChild(novoSpan)
+        }
+    }
+}
+
 
 
 botaoMostrarNumeros.addEventListener('click', mostrarNumeros)
 botaoMostrarNumerosPares.addEventListener('click', mostrarNumerosPares)
 botaoMostrarDobro.addEventListener('click', mostrarDobro)
 botaoMostrarPar.addEventListener('click', mostrarProximoPar)
+botaoMostrarNumerosMultiplos3Ou5.addEventListener('click', mostrarMultiplos3Ou5)
+botaoMostrarNumerosMultiplos3E5.addEventListener('click', mostrarMultiplos3E5)
+botaoPrimo.addEventListener('click', mostrarPrimo)
