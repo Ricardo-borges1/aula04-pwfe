3759ff010fa0e84a9c3eeb9ce14354f6d9d2c0da
wally 🧨🎇 <ricardoborges21122006@gmail.com>
2023-09-21 17:58:10 -0300

    projeto finalizado

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const botaoMostrarDobro = document.getElementById('botao-mostrar-dobro')
 const botaoMostrarPar = document.getElementById('botao-mostrar-proximo-par')
 const botaoMostrarNumerosMultiplos3Ou5 = document.getElementById('botao-mostrar-numeros-multiplos-3-ou-5')
 const botaoMostrarNumerosMultiplos3E5 = document.getElementById('botao-mostrar-numeros-multiplos-3-e-5')
-const botaoPrimo = document.getElementById('botao-primo')
+const botaoPrimo = document.getElementById('mostrar-primo')
 
 function mostrarNumeros() {
     const numeros = document.getElementById('numeros').value.split(',')
@@ -106,19 +106,46 @@ function mostrarMultiplos3E5 () {
     }
 }
 
-function mostrarPrimo() {
-    const numeros = document.getElementById('numeros').value.split(',')
-    const container = document.getElementById('container-primo')
+function primo() {
+    const numeros = document.getElementById('numeros').value.split(',');
+    const container = document.getElementById('cont-mostrar-primo');
 
-    container.replaceChildren('')
-    const ultimoIndice = numeros.length
+    container.replaceChildren();
 
-    for (let contador = 0; contador < ultimoIndice; contador++) {
-        if (numeros[contador] % 1 == 0 && numeros[contador] % valor == 0) {
-            const novoSpan = document.createElement('span')
-            novoSpan.textContent = numeros[contador] + 1
-            container.appendChild(novoSpan)
+    for (const numero of numeros) {
+        const valor = parseInt(numero);
+        const proximoPrimo = encontrarProximoPrimo(valor);
+
+        const novoSpan = document.createElement('span');
+        novoSpan.textContent = proximoPrimo;
+        container.appendChild(novoSpan);
+    }
+}
+
+function isPrimo(valor) {
+    if (valor <= 1) return false;
+    if (valor <= 3) return true;
+    if (valor % 2 === 0 || valor % 3 === 0) return false;
+
+    let i = 5;
+    while (i * i <= valor) {
+        if (valor % i === 0 || valor % (i + 2) === 0) {
+            return false;
         }
+        i += 6;
+    }
+
+    return true;
+}
+
+function encontrarProximoPrimo(valor) {
+    let proximo = valor + 1;
+
+    while (true) {
+        if (isPrimo(proximo)) {
+            return proximo;
+        }
+        proximo++;
     }
 }
 
@@ -130,4 +157,4 @@ botaoMostrarDobro.addEventListener('click', mostrarDobro)
 botaoMostrarPar.addEventListener('click', mostrarProximoPar)
 botaoMostrarNumerosMultiplos3Ou5.addEventListener('click', mostrarMultiplos3Ou5)
 botaoMostrarNumerosMultiplos3E5.addEventListener('click', mostrarMultiplos3E5)
-botaoPrimo.addEventListener('click', mostrarPrimo)
+botaoPrimo.addEventListener('click', primo)
